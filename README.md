3e4819ed949f105ef8039cd1a918474a51e3e3ac
gaoxinghua01 <gaoxinghua01@baidu.com>
2024-02-12 18:08:37 +0800

    完成加锁解锁功能

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # redis-lock
 redis分布式锁
+
+
+## SETNX 命令
++ 命令 SETNX 只有在 key 不存在时，才会设置 key 的值。
---
+++ lock.go
@@ -0,0 +1,74 @@
+package redis_lock
+
+import (
+	"context"
+	_ "embed"
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	ErrFailedToPreemptLock = errors.New("rlock: 抢锁失败")
+	ErrLockNotHold         = errors.New("rlock: 未持有锁")
+)
+
+type Client struct {
+	client redis.Cmdable
+}
+
+func NewClient(client redis.Cmdable) *Client {
+	return &Client{
+		client: client,
+	}
+}
+
+func (c *Client) TryLock(ctx context.Context, key string,
+	expiration time.Duration) (*Lock, error) {
+	val := uuid.New().String()
+	res, err := c.client.SetNX(ctx, key, val, expiration).Result()
+	if err != nil {
+		return nil, err
+	}
+	if !res {
+		return nil, ErrFailedToPreemptLock
+	}
+	return newLock(c.client, key, val), nil
+}
+
+var (
+	//go:embed script/lua/unlock.lua
+	luaUnLock string
+)
+
+type Lock struct {
+	key, value string
+	client     redis.Cmdable
+}
+
+func newLock(client redis.Cmdable, key, value string) *Lock {
+	return &Lock{
+		key:    key,
+		client: client,
+		value:  value,
+	}
+}
+
+func (l *Lock) UnLock(ctx context.Context) error {
+	// res, err := l.client.Del(ctx, l.key).Result()
+	// 解锁的时候需要确定锁还是自己的锁
+	//
+	res, err := l.client.Eval(ctx, luaUnLock, []string{l.key}, l.value).Int64()
+	if err == redis.Nil {
+		return ErrLockNotHold
+	}
+	if err != nil {
+		return err
+	}
+	if res != 1 {
+		return ErrLockNotHold
+	}
+	return nil
+}
